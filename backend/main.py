76ee766c38fadf69553c5059e2dafe72f8cd1cd1
Turki-Aldaajani <turki.aldajani11@gmail.com>
2026-03-09 17:11:14 +0300

    Update main.py (import  slowapi )

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 import io
 import os
 from dotenv import load_dotenv
